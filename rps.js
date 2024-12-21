14e95ce128b7171b1abaa4258650e3441aa24778
MaltaWebDev <MaltaWebDev@gmail.com>
2024-12-21 15:30:04 +0100

    Add function to get user input and index html file

--- rps.js
+++ rps.js
@@ -10,4 +10,19 @@ function getComputerChoice() {
     else
         return "scissors";
 }
-console.log(getComputerChoice());
+console.log("computer chooses: ", getComputerChoice());
+function getHumanChoice() {
+    var _a;
+    const humanChoice = (_a = prompt("Please enter rock, paper, or scissors (default is rock): ")) === null || _a === void 0 ? void 0 : _a.toLowerCase();
+    switch (humanChoice) {
+        case "rock":
+            return "rock";
+        case "scissors":
+            return "scissors";
+        case "paper":
+            return "paper";
+        default:
+            return "rock";
+    }
+}
+console.log(getHumanChoice());
--- rps.ts
+++ rps.ts
@@ -9,4 +9,22 @@ function getComputerChoice(): gameChoice {
     } else return "scissors";
 }
 
-console.log(getComputerChoice());
+console.log("computer chooses: ", getComputerChoice());
+
+function getHumanChoice(): gameChoice {
+    const humanChoice: string | undefined = prompt(
+        "Please enter rock, paper, or scissors (default is rock): "
+    )?.toLowerCase();
+    switch (humanChoice) {
+        case "rock":
+            return "rock";
+        case "scissors":
+            return "scissors";
+        case "paper":
+            return "paper";
+        default:
+            return "rock";
+    }
+}
+
+console.log(getHumanChoice());
